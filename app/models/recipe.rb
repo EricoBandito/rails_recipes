124916d4a4294691c13f31456cbe3916bf451e63
Eric Gagne <eric.jc.gagne@gmail.com>
2019-02-13 18:22:01 -0600

    Added model validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
+    validates :id, :title, presence: true
+    validates :title, length: { minimum: 5 }
 end
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,5 @@
 class RecipeIngredient < ApplicationRecord
     belongs_to :ingredient
     belongs_to :recipe
+    validates :id, :recipe_id, :ingredient_id, presence: true
 end
