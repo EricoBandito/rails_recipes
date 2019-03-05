edd0677e8fa61a9eec295ca04d5a9a5a116e6022
Eric Gagne <eric.jc.gagne@gmail.com>
2019-03-05 10:56:29 -0600

    Fixed faker seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,6 @@ uri = URI(url)
 response = Net::HTTP.get(uri)
 data = JSON.parse(response)['results']
 
-puts pp(data)
-
 Recipe.destroy_all
 
 data.each do |json_recipe|
@@ -26,6 +24,8 @@ data.each do |json_recipe|
 
     ingredient_array = json_recipe['ingredients'].split(',')
     ingredient_array.each do |ingredient|
+
+
 
         if Ingredient.exists?(:name => ingredient)
             ingredient_id  = Ingredient.find_by(name: ingredient).id
@@ -45,7 +45,7 @@ end
                            href: 'https://www.allrecipes.com/recipe/90236/ginger-champagne/',
                            thumbnail: '')
 
-    2..6.times do
+    3.times do
         ingredient = Faker::Food.ingredient
 
         if Ingredient.exists?(:name => ingredient)
@@ -57,7 +57,12 @@ end
             RecipeIngredient.create(recipe_id: recipe.id,
                                     ingredient_id: ingredient.id)
         end
-
     end
+
+
 end
+
+puts "Recipe count: #{Recipe.count()}"
+puts "Ingredient count: #{Ingredient.count()}"
+puts "RecipeIngredient count: #{RecipeIngredient.count()}"
 
